5f495703fa0616085b7ae26d5ebae3d51ab18f5b
alaabenhmida <ben.hmida.ala1@gmail.com>
2023-12-12 22:08:57 +0100

    minor changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import PageContent from '../components/PageContent';
 function HomePage() {
   return (
     <PageContent title="Welcome!">
-      <p>Browse all our amazing events!</p>
     </PageContent>
   );
 }
